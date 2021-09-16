4f87644a1c1ac53dffce379b298ff0a08c9e2257
drknsblct <57183092+drknsblct@users.noreply.github.com>
2021-09-17 02:20:01 +0300

    Update exercise.py

--- src/exercise.py
+++ src/exercise.py
@@ -1,5 +1,24 @@
 def main():
     #write your code below this line
+    num1 = int(input('Give the first number:'))
+    num2 = int(input('Give the second number:'))
+    
+    
+    print(str(num1) + ' + ' + str(num2) + ' = ' + str(num1+num2))
+    print(str(num1) + ' - ' + str(num2) + ' = ' + str(num1-num2))
+    print(str(num1) + ' * ' + str(num2) + ' = ' + str(num1*num2))
+    print(str(num1) + ' / ' + str(num2) + ' = ' + str(num1/num2))
+    
 
 if __name__ == '__main__':
     main()
+
+    
+    Give the first number:
+*8*
+Give the second number:
+*2*
+8 + 2 = 10
+8 - 2 = 6
+8 * 2 = 16
+8 / 2 = 4.0
